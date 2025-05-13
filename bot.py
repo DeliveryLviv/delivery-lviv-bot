ac6bc2b46acb97b84e97363fc69c811b481517b5
DeliveryLviv <crevedkao@gmail.com>
2025-05-14 01:19:41 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -111,6 +111,7 @@ if __name__ == '__main__':
 
     async def main():
         port = int(os.environ.get("PORT", 5000))
+        app.run(host="0.0.0.0", port=port)
         await application.initialize()
         await application.start()
         await bot.set_webhook(WEBHOOK_URL)
